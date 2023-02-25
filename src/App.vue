480cd5b276918a2b808a3a852bceaf3ed7e33d75
Tea <1518014096@qq.com>
2023-02-25 15:21:43 +0800

    pomelo-vuex

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>Test</div>
+    <div>{{ $store.state.number }}</div>
+    <div>{{ $store.getters.numberAddOne }}</div>
+    <button
+        style="width: 200px;background-color: pink;"
+        @click="addNumber(10)">
+      Add Number(mutation)
+    </button>
+    <button
+        style="width:200px;background-color: red;"
+        @click="addNumberAsync(100)">
+      Add Number(action)
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    addNumber(number) {
+      this.$store.commit('addNumber', number)
+    },
+    addNumberAsync(number) {
+      this.$store.dispatch('addNumberAsync', number)
+    }
+  }
+}
+</script>
